80b9e7fe19fd0b5f16ecceb9f9c1c18cb1b93c17
George Ascherl <georgea@cox.net>
2015-11-02 17:10:19 -0600

    first attempt

---
+++ app.js
@@ -0,0 +1,44 @@
+var pikePlaceMarket = {
+  minhourcust: 17,
+  maxhourcust: 88,
+  avecookiecust 5.2,
+  hourTotals: [],
+  dailyTotal: 0,
+  randomCustHour: function() {
+    return Math.floor(Math.random() * (maxhourcust - minhourcust + 1)
+      + minhourcust);
+  }
+
+  cookiesPerHour: function() {
+    return randomCustHour * avecookiecust
+  }
+
+};
+
+var seaTacAir = {
+  minhourcust: 6,
+  maxhourcust: 44,
+  avecookiecust 1.2,
+
+};
+
+var southMall = {
+  minhourcust: 11,
+  maxhourcust: 38,
+  avecookiecust 1.9,
+
+};
+
+var bellevueSquare = {
+  minhourcust: 20,
+  maxhourcust: 48,
+  avecookiecust 3.3,
+
+};
+
+var alki = {
+  minhourcust: 3,
+  maxhourcust: 24,
+  avecookiecust 2.6,
+
+};
