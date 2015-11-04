690cd0786665d6b2bb9962d1e4a814714c44e704
George Ascherl <georgea@cox.net>
2015-11-04 15:47:06 -0800

    fixed js for one shop and added google font

--- app.js
+++ app.js
@@ -1,85 +1,89 @@
-var hours = ['10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm'
+var hours = ['10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm']
 
 var pikePlaceMarket = {
   minHourCust: 17,
-  minHourCust: 88,
-  aveCookieCust 5.2,
+  maxHourCust: 88,
+  aveCookieCust: 5.2,
   hourTotals: [],
   dailyTotal: 0,
 
   randomCustHour: function() {
-    return Math.floor(Math.random() * (this.minHourCust - this.minHourCust + 1)
+    return Math.floor(Math.random() * (this.maxHourCust - this.minHourCust + 1)
       + this.minHourCust);
   },
 
   cookiesPerHour: function() {
-    for (var i = 0; i < hours.length, i++) {
-    var cookiesPerHour = Math.floor(this.aveCookieCust * this.randomCustHour());
-    console.log('The number of cookies purchased for each hour is ' + cookiesPerHour);
-    this.hourTotals.push(cookiesPerHour);
-    this.dailyTotal += cookiesPerHour;
-    console.log('The sum of these hourly totals is ' + this.dailyTotal);
+    for (var i = 0; i < hours.length; i++) {
+      var cph = Math.floor(this.aveCookieCust * this.randomCustHour());
+      console.log('The number of cookies purchased for each hour is ' + cph);
+      this.hourTotals.push(cph);
+      this.dailyTotal += cph;
+      console.log('The sum of these hourly totals is ' + this.dailyTotal);
+    }
   },
 
   makeList: function() {
     this.cookiesPerHour();
+    var pike = document.getElementById('pike');
 
     for (var i = 0; i < hours.length; i++) {
-      var list = document.createElement('list');
-      list.textContent = hours[i] + ': ' + this.hourTotals[i];
-      pikeUl.appendChild(list);
+      var li = document.createElement('li');
+      li.textContent = hours[i] + ': ' + this.hourTotals[i];
+      pike.appendChild(li);
     }
-    var list = document.createElement('list');
-    list.textContent = 'Daily Total: ' + this.dailyTotal;
-    pikeUl.appendChild(list);
+
+    var li2 = document.createElement('li');
+    li2.textContent = 'Daily Total: ' + this.dailyTotal;
+    pike.appendChild(li2);
 
   }
 };
 
-var seaTacAir = {
-  minHourCust: 6,
-  minHourCust: 44,
-  aveCookieCust 1.2,
-  hourTotals: [],
-  dailyTotal: 0,
+pikePlaceMarket.makeList();
+// var seaTacAir = {
+//   minHourCust: 6,
+//   minHourCust: 44,
+//   aveCookieCust 1.2,
+//   hourTotals: [],
+//   dailyTotal: 0,
 
-};
+// };
 
-var southMall = {
-  minHourCust: 11,
-  minHourCust: 38,
-  aveCookieCust 1.9,
-  hourTotals: [],
-  dailyTotal: 0,
+// var southMall = {
+//   minHourCust: 11,
+//   minHourCust: 38,
+//   aveCookieCust 1.9,
+//   hourTotals: [],
+//   dailyTotal: 0,
 
-};
+// };
 
-var bellevueSquare = {
-  minHourCust: 20,
-  minHourCust: 48,
-  aveCookieCust 3.3,
-  hourTotals: [],
-  dailyTotal: 0,
+// var bellevueSquare = {
+//   minHourCust: 20,
+//   minHourCust: 48,
+//   aveCookieCust 3.3,
+//   hourTotals: [],
+//   dailyTotal: 0,
 
-};
+// };
 
-var alki = {
-  minHourCust: 3,
-  minHourCust: 24,
-  aveCookieCust 2.6,
-  hourTotals: [],
-  dailyTotal: 0,
+// var alki = {
+//   minHourCust: 3,
+//   minHourCust: 24,
+//   aveCookieCust 2.6,
+//   hourTotals: [],
+//   dailyTotal: 0,
 
-};
+// };
 
-var pikeUl = getElementById('pike')
-var seaTacUl = getElementById('seaTac')
-var southCenterUl = getElementById('southCenter')
-var bellevueSquareUl = getElementById('bellevueSquare')
-var alkiUL = getElementById('alki')
+// var pikeUl = getElementById('pike')
+// var seaTacUl = getElementById('seaTac')
+// var southCenterUl = getElementById('southCenter')
+// var bellevueSquareUl = getElementById('bellevueSquare')
+// var alkiUL = getElementById('alki')
 
-pikePlaceMarket.makeList()
-seaTacAir.makeList()
-southMall.makeList()
-bellevueSquare.makeList()
-alki.makeList()
+// pikePlaceMarket.makeList()
+// seaTacAir.makeList()
+// southMall.makeList()
+// bellevueSquare.makeList()
+// alki.makeList()
