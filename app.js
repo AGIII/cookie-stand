2eae9c75b83124100b998fd824b944daa9741d43
George Ascherl <georgea@cox.net>
2015-11-05 16:58:34 -0800

    started wiring up form, still not working

--- app.js
+++ app.js
@@ -1,6 +1,31 @@
 var hours = ['10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm']
-
 var tbl = document.createElement('table');
+var form = document.getElementById('location-form');
+
+
+// var handleForm = function(event) {
+//   event.preventDefault();
+//   console.log(event);
+//   console.log(event.target.mincust.value);
+
+
+
+// var loc = event.target.locationame.value;
+// var min = event.target.mincust.value;
+// var max = event.target.maxcust.value;
+// var avg = event.target.avgcust.value;
+
+
+// //this is where I am trying to append the new row for the form
+// var newshoprow = document.createElement('tr');
+
+
+
+//   //import the value and set to a variable
+//   //create a new cookiestand object with those variables
+//   //append a row representing that cooking stand onto the end of the table
+// }
+
 
 function CookieStand (location, minHourCust, maxHourCust, aveCookieCust) {
   this.location = location;
@@ -66,8 +91,13 @@ var southCenter = new CookieStand("South Center Mall", 11, 38, 1.9);
 var bellevueSquare = new CookieStand("Bellevue Square Mall", 20, 48, 3.3);
 var alki = new CookieStand("Alki Beach", 3, 24, 2.6);
 
+// //this is the variable I have created for the new shop from the form
+// var newShop = new CookieStand(loc, min, max, avgcust);
+
 pikePlaceMarket.makeList();
 seaTac.makeList();
 southCenter.makeList();
 bellevueSquare.makeList();
 alki.makeList();
+
+form.addEventListener("submit", handleForm)
